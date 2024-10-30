2d29ec05ca79baad070caa2aa39ad5811d876b86
copycode69 <jaytiwari092@gmail.com>
2024-10-30 21:35:21 +0530

    update bottles.js

--- 1-ecmascript/03-99-bottles/99-bottles.js
+++ 1-ecmascript/03-99-bottles/99-bottles.js
@@ -2,9 +2,9 @@
 // Codédex
 
 for (let i = 99; i > 0; i--) {
-  console.log(`${i} bottles of pop on the wall`);
-  console.log(`${i} bottles of pop`);
+  console.log(`${i} ${i === 1 ? 'bottle' : 'bottles'} of pop on the wall`);
+  console.log(`${i} ${i === 1 ? 'bottle' : 'bottles'} of pop`);
   console.log("take one down, pass it around");
-  console.log(`${i - 1} bottles of pop on the wall`);
+  console.log(`${i - 1} ${i - 1 === 1 ? 'bottle' : 'bottles'} of pop on the wall`);
   console.log();
 }
